e0da56b894799858651e4d95c13778a252d10040
Ramnesh Bansal <ramnesh.bansal@gmail.com>
2011-02-13 11:29:10 -0500

    Added a User controller and layout link tests

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 SampleApp::Application.routes.draw do
   
+  get "users/new"
+
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
   
@@ -8,6 +10,7 @@ SampleApp::Application.routes.draw do
   match '/contact', :to => 'pages#contact' 
   match '/about', :to => 'pages#about' 
   match '/help', :to => 'pages#help' 
+  match '/signup', :to => 'users#new' 
     
   get "pages/home"
   #get "pages/contact"
