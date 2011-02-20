d9a5fa8ea91cfb9fca7185f360c53e98ebdc5fb9
Ramnesh Bansal <ramnesh.bansal@gmail.com>
2011-02-20 07:46:41 -0500

    Added Sign-up form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,10 +3,10 @@ class UsersController < ApplicationController
   def show
 	@user = User.find(params[:id])
 	@title = @user.name
-	#@title = @user.title
   end
 
   def new
+	@user = User.new
 	@title = "Sign up"
   end
 
