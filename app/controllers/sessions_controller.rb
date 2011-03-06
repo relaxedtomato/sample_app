dfac279b4dfa540b41fae6cca5081cc8e5019fff
Ramnesh Bansal <ramnesh.bansal@gmail.com>
2011-03-06 08:04:01 -0500

    Completed signin/signout

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,7 +5,7 @@ class SessionsController < ApplicationController
   
   def create
     user = User.authenticate(params[:session][:email],
-							params[:sessions][:password])
+							params[:session][:password])
 	if user.nil?
 	  flash.now[:error] = "Invalid email/password combination."	
 	  @title = "Sign in"
@@ -17,7 +17,8 @@ class SessionsController < ApplicationController
   end
   
   def destroy
-	
+	sign_out
+	redirect_to root_path
   end
 
 end
