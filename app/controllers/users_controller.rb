6841c5128eaa2639f755aff649475721cac731a1
Ramnesh Bansal <ramnesh.bansal@gmail.com>
2011-03-10 07:19:33 -0500

    Friendly forwarding and correct user requirement

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
 
   before_filter :authenticate, :only => [:edit, :update]
+  before_filder :correct_user, :only => [:edit, :update]
 
   def show
 	@user = User.find(params[:id])
@@ -24,6 +25,7 @@ class UsersController < ApplicationController
   end
   
   def edit
+	#raise request.inspect
 	@user = User.find(params[:id])
 	@title = "Edit user"
   end
@@ -44,9 +46,9 @@ class UsersController < ApplicationController
 	#flash[:notice] = "Please sign in to access this page."
 	deny_access unless signed_in?
   end
-  
-  def deny_access
-	redirect_to signin_path, :notice => "Please sign in to access this page." 
+   
+  def correct_user
+    @user = User.find(params[:id])
+	redirect_to(root_path) unless @user == current_user
   end
-  
 end
